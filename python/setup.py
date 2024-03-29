7d108e15e0fa95b2ea8a4ee401912227a0ba368f
appeacock <alawson@aqorn.com>
2024-03-29 10:55:12 -0700

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 from setuptools import setup
 
-#Install phonetisaurus 
-setup (
-    name         = 'phonetisaurus',
-    version      = '0.3',
-    description  = 'Phonetisaurus G2P python package (OpenFst-1.6.x)',
-    url          = 'http://code.google.com/p/phonetisaurus',
-    author       = 'Josef Novak',
-    author_email = 'josef.robert.novak@gmail.com',
-    license      = 'BSD',
-    packages     = ['phonetisaurus'],
-    package_data = {'' : ['Phonetisaurus.so']},
-    include_package_data = True,
-    install_requires = ["argparse", "bottle"],
-    zip_safe     = False
+# Install phonetisaurus 
+setup(
+    name='phonetisaurus',
+    version='0.3',
+    description='Phonetisaurus G2P python package (OpenFst-1.6.x)',
+    url='http://code.google.com/p/phonetisaurus',
+    author='Josef Novak',
+    author_email='josef.robert.novak@gmail.com',
+    license='BSD',
+    packages=['phonetisaurus'],
+    package_data={'': ['Phonetisaurus.so']},
+    include_package_data=True,
+    install_requires=["bottle"],  # Removed argparse as it's part of the standard library in Python 3
+    zip_safe=False
 )
